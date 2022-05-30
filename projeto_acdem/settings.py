011dfbf22b58bd78e450bd5963e540ec8506626b
Carlos Mesquita <carlossmessquita@gmail.com>
2022-05-29 21:41:36 -0300

    Configurar Django/Heroku

--- projeto_acdem/settings.py
+++ projeto_acdem/settings.py
@@ -1,5 +1,6 @@
 import os.path
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -121,3 +122,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
