36b8883de908fffd1f364e2fed1d2e2da72be252
Carlos Mesquita <carlossmessquita@gmail.com>
2022-05-29 21:20:55 -0300

    Atualziando debug

--- projeto_acdem/settings.py
+++ projeto_acdem/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-87*^&f9y+)sqeg59&n^rcq##!m7)2a_#57^3!o35twi__2)m@&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
